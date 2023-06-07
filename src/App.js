241b3c12d828977f5bc4e220326b4d2449a24566
dobob777 <dobob777@gmail.com>
2023-06-07 18:54:48 +0530

    Table-API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log('data::: ', data);
+
+  useEffect(() => {
+    const Hello = async () => {
+      fetch('https://dummyjson.com/todos')
+        .then(res => res.json())
+        .then((todo) => {
+          setData(todo.todos);
+        })
+        .catch(error =>
+          console.log('error::: ', error)
+        )
+    }
+    Hello();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table className="results">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>UserId</th>
+            <th>todo</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.slice(0, 10).map((ele, index) => (
+              <tr key={ index }>
+                <td>{ ele.id }</td>
+                <td>{ ele.userId }</td>
+                <td>{ ele.todo }</td>
+                <td>{
+                  ele.completed ?
+                    <span className="tag green">Completed</span>
+                    :
+                    <span className="tag red">Not Completed</span>
+                }</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+    </>
   );
 }
 
